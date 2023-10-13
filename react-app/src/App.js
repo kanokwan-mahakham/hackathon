0c4e60a6e10c4da7670efba425aa2d8042e6fd9e
ariss999 <87415367+ariss999@users.noreply.github.com>
2023-10-13 22:23:49 +0700

    set route

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,22 @@
+import EditHome from './features/Home/EditHome';
+import HomePage from './features/Home/HomePage'
+import LoginUser from './features/Login/LoginUser'
+import RegisterUser from './features/Registers/RegisterPage/Register-user'
+import RegisterCompany from './features/Registers/RegisterPage/Register-company';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <></>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/edit-home" element={<EditHome />} />
+        <Route path="/login" element={<LoginUser />} />
+        <Route path="/register-user" element={<RegisterUser />} />
+        <Route path="/register-company" element={<RegisterCompany />} />
+      </Routes>
+    </>
   );
 }
 
 export default App;
-
-
