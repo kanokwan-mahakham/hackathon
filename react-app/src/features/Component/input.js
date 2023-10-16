220d7c18bff523c27cb4a7447ec637ee18ae40b4
Supattha <118960003+Supattha@users.noreply.github.com>
2023-10-17 01:41:09 +0700

    edit reg

--- react-app/src/features/Component/input.js
+++ react-app/src/features/Component/input.js
@@ -3,30 +3,24 @@ import styled from 'styled-components';
 
 
 const InputContainer = styled.div`
-display: flex;
-height: 34.65px;
-padding: 5.775px 8.663px;
-align-items: center;
-gap: 100px;
-border-radius: 8.663px;
-border: 0.722px solid #DBDBDB;
-margin-top: 10px;
-flex-direction: row;
-flex-wrap: nowrap;
-align-content: flex-end;
-justify-content: space-between;
-align-items: center;
+.input-container2{display: flex;
+    height: 34.65px;
+    padding: 5.775px 8.663px;
+    align-items: center;
+    gap: 10.425px;
+    border-radius: 8.663px;
+    border: 0.722px solid #DBDBDB;
+    margin-top: 5px;}
 `;
-
 
 const Input = styled.input`
 color: #807D7D;
-font-family: 'Open Sans';
-font-size: 11.55px;
-font-weight: 400;
-line-height: 17.325px;
-border: none;
-outline: none;
+    font-family: 'Open Sans';
+    font-size: 11.55px;
+    font-weight: 400;
+    line-height: 17.325px;
+    border: none;
+    outline: none;
 `;
 
 const IconContainer = styled.div`
@@ -36,7 +30,7 @@ const IconContainer = styled.div`
 `;
 
 
-const InputRegField = ({ placeholder, type,onChange, icon }) => {
+const InputField = ({ placeholder, type,onChange, icon }) => {
 
     const [newType,setNewType] = useState(type);
     const [newIcon,setNewIcon] = useState(icon);
@@ -54,10 +48,14 @@ const InputRegField = ({ placeholder, type,onChange, icon }) => {
 
     return (
         <InputContainer>
+            <div className="input-container2">
             <Input type={newType} placeholder={placeholder} onChange={onChange} />
             {icon && <IconContainer onClick={changeType}>{newIcon}</IconContainer>}
+            </div>
         </InputContainer>
+
+
     );
 };
 
-export default Input;
+export default InputField;
--- react-app/src/features/Registers/RegisterPage/Register-user/index.js
+++ react-app/src/features/Registers/RegisterPage/Register-user/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import InputRegField from '../../../Component/input';
+import InputField from '../../../Component/input';
 import Button from '../../../Component/Botton';
 import styled from 'styled-components';
 import { Link,useNavigate } from "react-router-dom";
@@ -287,20 +287,20 @@ const RegisterUser = ({setUser,url,className}) => {
                     </div>
                     </StyledGroupline>
 
-                    <InputRegField placeholder="Enter your email" type="text" onChange={(event) => {setEmail(event.target.value);}}/>
+                    <InputField placeholder="Enter your email" type="text" onChange={(event) => {setEmail(event.target.value);}}/>
 
-                            <InputRegField placeholder="Create password" type="password" icon={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                            <InputField placeholder="Create password" type="password" icon={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
                                 <path d="M7.8686 10.6558C6.43086 10.6558 5.26383 9.48879 5.26383 8.05105C5.26383 6.61332 6.43086 5.44629 7.8686 5.44629C9.30633 5.44629 10.4734 6.61332 10.4734 8.05105C10.4734 9.48879 9.30633 10.6558 7.8686 10.6558ZM7.8686 6.34863C6.93016 6.34863 6.16618 7.11262 6.16618 8.05105C6.16618 8.98949 6.93016 9.75348 7.8686 9.75348C8.80704 9.75348 9.57102 8.98949 9.57102 8.05105C9.57102 7.11262 8.80704 6.34863 7.8686 6.34863Z" fill="#828C97"/>
                                 <path d="M7.86857 13.4772C5.60669 13.4772 3.47115 12.1538 2.00334 9.85578C1.36568 8.8632 1.36568 7.245 2.00334 6.24641C3.47716 3.94844 5.61271 2.625 7.86857 2.625C10.1244 2.625 12.26 3.94844 13.7278 6.24641C14.3654 7.23898 14.3654 8.85719 13.7278 9.85578C12.26 12.1538 10.1244 13.4772 7.86857 13.4772ZM7.86857 3.52734C5.92552 3.52734 4.06669 4.69438 2.76732 6.73367C2.31615 7.4375 2.31615 8.66469 2.76732 9.36852C4.06669 11.4078 5.92552 12.5748 7.86857 12.5748C9.81162 12.5748 11.6704 11.4078 12.9698 9.36852C13.421 8.66469 13.421 7.4375 12.9698 6.73367C11.6704 4.69438 9.81162 3.52734 7.86857 3.52734Z" fill="#828C97"/>
                             </svg>} onChange={(event) => {setPassword(event.target.value);}} />
 
-                            <InputRegField placeholder="Confirm password" type="password" icon={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                            <InputField placeholder="Confirm password" type="password" icon={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
                                 <path d="M7.8686 10.6558C6.43086 10.6558 5.26383 9.48879 5.26383 8.05105C5.26383 6.61332 6.43086 5.44629 7.8686 5.44629C9.30633 5.44629 10.4734 6.61332 10.4734 8.05105C10.4734 9.48879 9.30633 10.6558 7.8686 10.6558ZM7.8686 6.34863C6.93016 6.34863 6.16618 7.11262 6.16618 8.05105C6.16618 8.98949 6.93016 9.75348 7.8686 9.75348C8.80704 9.75348 9.57102 8.98949 9.57102 8.05105C9.57102 7.11262 8.80704 6.34863 7.8686 6.34863Z" fill="#828C97"/>
                                 <path d="M7.86857 13.4772C5.60669 13.4772 3.47115 12.1538 2.00334 9.85578C1.36568 8.8632 1.36568 7.245 2.00334 6.24641C3.47716 3.94844 5.61271 2.625 7.86857 2.625C10.1244 2.625 12.26 3.94844 13.7278 6.24641C14.3654 7.23898 14.3654 8.85719 13.7278 9.85578C12.26 12.1538 10.1244 13.4772 7.86857 13.4772ZM7.86857 3.52734C5.92552 3.52734 4.06669 4.69438 2.76732 6.73367C2.31615 7.4375 2.31615 8.66469 2.76732 9.36852C4.06669 11.4078 5.92552 12.5748 7.86857 12.5748C9.81162 12.5748 11.6704 11.4078 12.9698 9.36852C13.421 8.66469 13.421 7.4375 12.9698 6.73367C11.6704 4.69438 9.81162 3.52734 7.86857 3.52734Z" fill="#828C97"/>
                             </svg>}  onChange={(event) => {setComfirmPassword(event.target.value);}}/>
 
 
-                            <InputRegField placeholder="Username" type="text" onChange={(event) => {setUsername(event.target.value);}}/>
+                            <InputField placeholder="Username" type="text" onChange={(event) => {setUsername(event.target.value);}}/>
 
                            
                     <StyledBotton>
